8c47ad3d82ff8fe2ea3f0533ccb1684817e8fc73
sheersh1111 <106367218+sheersh1111@users.noreply.github.com>
2023-04-23 12:31:53 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,10 @@ app.use(express.urlencoded({limit:"50mb",extended:true}));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(fileUpload());
 
+app.use(express.static(path.join(__dirname,"../frontend/build")));
+app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
+})
 
 
 //route imports
